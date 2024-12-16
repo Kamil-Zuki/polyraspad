7762585601b03aa5675b3b92788f4afadeb30157
Zuko <karatov7@gmail.com>
2024-12-17 00:40:45 +0300

    feat: add daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+    <div className="m-4">
+      <h1 className="text-4xl">Hello</h1>
+      <button className="btn btn-primary">Add to card</button>
+    </div>
     </>
     
   );
