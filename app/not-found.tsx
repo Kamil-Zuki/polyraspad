89d3bc8db781ea6a412b2a9c3726d252b4adec03
Zuko <karatov7@gmail.com>
2024-12-17 19:43:46 +0300

    feat: add the not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return (
+      <div className="text-red-800 flex justify-center items-center min-h-screen">
+        <h2 className="text-3xl font-semibold">404 Not Found</h2>
+      </div>
+    );
+  };
+  
+  export default NotFoundPage;
