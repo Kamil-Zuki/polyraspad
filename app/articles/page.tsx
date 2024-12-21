eb4859460e13b86fea63f80446da9e7b20064528
Zuko <karatov7@gmail.com>
2024-12-22 00:57:13 +0300

    feat: remove project files

--- app/articles/page.tsx
+++
@@ -1,9 +0,0 @@
-const ArticlesPage = () =>{
-    return(
-        <div className="flex justify-center items-center min-h-screen">
-            <h2 className="text-3xl font-semibold">Welcome to Articles section</h2>
-        </div>
-    );
-};
-
-export default ArticlesPage;
